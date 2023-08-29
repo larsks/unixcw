888bd3b224b3cfc86bbfb0be9046316e5ba7eaaf
acerion <acerion@wp.pl>
2023-08-29 21:35:08 +0200

    Resolving a TODO about storage of strings
    Double-checking that string literals have static storage and have enough
    lifetime to be safe in this context.

--- src/test_framework/basic_utils/test_result.c
+++ src/test_framework/basic_utils/test_result.c
@@ -41,12 +41,8 @@ const char * get_test_result_string(test_result_t result)
 #define BEGIN_RED     "\x1B[31m"
 #define END_COLOR     "\x1B[0m"
 
-	/*
-	  TODO (acerion) 2023.08.15 This function assumes that the string
-	  literals are kept on heap, or in other non-stack location. Check this
-	  expectation is following a standard or just uses an
-	  implementation-specific behaviour.
-	*/
+	/* String literals with 'static storage duration'
+	   (https://en.cppreference.com/w/c/language/string_literal). */
 	switch (result) {
 	case test_result_pass:
 		return BEGIN_GREEN"[PASS]"END_COLOR;
