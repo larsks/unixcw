ea042baa91ade611bca921cf2814fc29d3dc7caf
acerion <acerion@wp.pl>
2023-09-17 13:25:32 +0200

    Reviewing and improving qa script that tests ./configure options

--- qa/test_configure_flags.sh
+++ qa/test_configure_flags.sh
@@ -22,13 +22,23 @@
 
 
 
-# TODO: the path to bash on FreeBSD is /usr/local/bin/bash
 
-# Test configuration and compilation of software for all combinations of
-# './configure' options that configure features of unixcw package.
+# Test configuration and compilation of software for (almost) all
+# combinations of './configure' options that configure features of unixcw
+# package.
 #
 # The tests are performed in order to find compilation errors caused
 # by conditional #includes and #defs in source code.
+#
+# unixcw's ./configure script accepts 8 options that are relevant to user's
+# scenarios. I can't test all those combinations manually and ensure that
+# none of them results in compilation erorr, hence the script.
+#
+# Options that are unlikely to be used by end-user (i.e. options that are
+# specific to development activities) can be omitted from this test. If
+# enabling them causes any compilation problems, the problems will be
+# detected during development. This omission is intended to shorten time
+# needed to execute this script.
 #
 # Combinations of options are numbered and executed from
 # 2^total_options_count to 1 (where total_options_count is the number of
@@ -39,10 +49,15 @@
 
 
 
+# TODO: the path to bash on FreeBSD is /usr/local/bin/bash
+
+
+
+
 let make_jobs=2
 
 # All interesting options that can be passed to ./configure.
-let total_options_count=11
+let total_options_count=9
 all_options[0]="--disable-console"
 all_options[1]="--disable-oss"
 all_options[2]="--disable-alsa"
@@ -51,9 +66,13 @@ all_options[4]="--disable-cwgen"
 all_options[5]="--disable-cw"
 all_options[6]="--disable-cwcp"
 all_options[7]="--disable-xcwcp"
-all_options[8]="--enable-dev-receiver-test"
-all_options[9]="--enable-dev-libcw-debugging"
-all_options[10]="--enable-dev-pcm-samples-file"
+# His development opion *perhaps* *may* be used by end-user if end user will
+# want to debug something.
+all_options[8]="--enable-dev-libcw-debugging"
+# These options are unlikely to be used by users, so don't test them.
+# Excluding them decreases the total time needed to run this script.
+# all_options[]="--enable-dev-receiver-test"
+# all_options[]="--enable-dev-pcm-samples-file"
 
 
 
@@ -71,6 +90,43 @@ fi
 
 
 
+function get_date()
+{
+	echo `date +%Y_%m_%d__%H_%M_%S`
+}
+
+
+
+
+start_ts=$(get_date)
+
+# If anything goes wrong during a long test, then at least save a log of
+# executed iterations in the log file. The repo may be placed in tmpfs and
+# this test script may be executed in there, so put the log in ~/ dir which
+# has a high chance of being a permanent localization.
+log_file=~/test_configure_flags_log_$start_ts.txt
+
+
+
+
+function debug()
+{
+	echo "[DEBUG] $1" | tee -a $log_file
+}
+
+function info()
+{
+	echo "[INFO ] $1" | tee -a $log_file
+}
+
+function error()
+{
+	echo "[ERROR] $1" | tee -a $log_file
+}
+
+
+
+
 # Get a string with a list/combination of options passed to ./configure. The
 # combination corresponds to value of first argument: a value in range of
 # (<total count of possibilities> to <1>).
@@ -84,7 +140,7 @@ function get_options()
 	local combination=$2
 	local result=""
 
-	# echo "[DEBUG] n_options = $n_options, combination = $combination"
+	# debug "n_options = $n_options, combination = $combination"
 
 	# This loop is a method of determining what digit stands at a leftmost
 	# position of binary representation of $combination.
@@ -120,14 +176,19 @@ function get_options()
 
 
 
+info "Start time: $start_ts"
+info "Initial iteration number: $i"
 make clean &> /dev/null
+
+
+
 
 for ((; i>0; i--))
 do
 	options=$(get_options $total_options_count $i)
 
 	# Test code.
-	# echo "[INFO ] Options = $options"
+	# info "Options = $options"
 	# continue
 
 	# The main part - the compilation with given configuration options.
@@ -139,13 +200,15 @@ do
 	#    heavy-duty test of 'make dist')
 	# Unfortunately "make distcheck" takes more time than just "make check".
 	command="./configure $options &>/dev/null && make -j $make_jobs &>/dev/null && make check -j $make_jobs &>/dev/null && make clean -j $make_jobs &>/dev/null"
-	# echo "[DEBUG] Command: $command"
-	echo "[INFO ] Iteration $i: $options"
+	# debug "Command: $command"
+	info "Iteration $i: $options"
 	result=$(eval $command)
 
 	# $? is the result code of last command.
 	if [ $? != 0 ]; then
-		echo "[ERROR] Test of configuration options FAILED in iteration $i for these options: $options"
+		error "Test of configuration options FAILED in iteration $i for these options: $options"
+		end_ts=$(get_date)
+		info "End time: $end_ts"
 		exit -1
 	fi
 done
@@ -153,6 +216,8 @@ done
 
 
 
-echo "[INFO ] Test of entire space of configuration options has SUCCEEDED"
+info "Test of entire space of configuration options has SUCCEEDED"
+end_ts=$(get_date)
+info "End time: $end_ts"
 exit 0
 
